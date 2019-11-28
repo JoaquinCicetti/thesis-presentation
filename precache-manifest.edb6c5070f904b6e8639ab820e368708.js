cf59b7294802b8e1d0d088f09cbbd7edb887453b
Joaquin <joaquincicetti@gmail.com>
2019-11-28 19:41:53 -0300

    Updates

---
+++ precache-manifest.edb6c5070f904b6e8639ab820e368708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24edf998e61d6e1605de05a2c639df2",
+    "url": "/thesis-presentation/index.html"
+  },
+  {
+    "revision": "776ff9b16a0c793847c1",
+    "url": "/thesis-presentation/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "adb9e71be6ac032305e2",
+    "url": "/thesis-presentation/static/css/main.fdd24090.chunk.css"
+  },
+  {
+    "revision": "776ff9b16a0c793847c1",
+    "url": "/thesis-presentation/static/js/2.89808b7a.chunk.js"
+  },
+  {
+    "revision": "adb9e71be6ac032305e2",
+    "url": "/thesis-presentation/static/js/main.d7c4fe36.chunk.js"
+  },
+  {
+    "revision": "6204334d5740ce958a6b",
+    "url": "/thesis-presentation/static/js/runtime-main.5a70276e.js"
+  },
+  {
+    "revision": "6521d7ea78448ebbe4bc9d5d634e4079",
+    "url": "/thesis-presentation/static/media/Exo2.0-Light.6521d7ea.otf"
+  }
+]);

ef86537924fff28d4d3a8902ce59ab518e5b3b2a
Joaquin <joaquincicetti@gmail.com>
2019-12-10 15:02:55 -0300

    Updates

--- precache-manifest.8a3d78f9f2f5de42d6ed1b9386d13bfa.js
+++ precache-manifest.8a3d78f9f2f5de42d6ed1b9386d13bfa.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b84dcc8a6308b4314a8f46ca4e15725",
+    "revision": "db6e12101d4b41aeab687036395263df",
     "url": "/thesis-presentation/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/thesis-presentation/static/css/2.1af7a3c5.chunk.css"
   },
   {
-    "revision": "d140f0508a4f2e868c75",
-    "url": "/thesis-presentation/static/css/main.77050afa.chunk.css"
+    "revision": "3a696f9494d344459031",
+    "url": "/thesis-presentation/static/css/main.17930c2d.chunk.css"
   },
   {
     "revision": "0770e80141341fd26ede",
     "url": "/thesis-presentation/static/js/2.f3ea10b6.chunk.js"
   },
   {
-    "revision": "d140f0508a4f2e868c75",
-    "url": "/thesis-presentation/static/js/main.af12d95d.chunk.js"
+    "revision": "3a696f9494d344459031",
+    "url": "/thesis-presentation/static/js/main.164be3c9.chunk.js"
   },
   {
     "revision": "6204334d5740ce958a6b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thesis-presentation/precache-manifest.e82635bbd5ec3d469ffc6081680ca126.js"
+  "/thesis-presentation/precache-manifest.8a3d78f9f2f5de42d6ed1b9386d13bfa.js"
 );
 
 self.addEventListener('message', (event) => {

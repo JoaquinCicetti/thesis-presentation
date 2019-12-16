bc5785cff9e4eb46fdb9ba9024fbc2952ed6a3a6
Joaquin <joaquincicetti@gmail.com>
2019-12-16 12:40:01 -0300

    Updates

---
+++ precache-manifest.cfbd8108b560760e448ebf5d9459211f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c923163e2453ff3ef2e2d0ff23c2ec",
+    "url": "/thesis-presentation/index.html"
+  },
+  {
+    "revision": "e885363f927980c043a7",
+    "url": "/thesis-presentation/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "4c4c97919065af899960",
+    "url": "/thesis-presentation/static/css/main.13536f7b.chunk.css"
+  },
+  {
+    "revision": "e885363f927980c043a7",
+    "url": "/thesis-presentation/static/js/2.41227040.chunk.js"
+  },
+  {
+    "revision": "4c4c97919065af899960",
+    "url": "/thesis-presentation/static/js/main.12975803.chunk.js"
+  },
+  {
+    "revision": "6204334d5740ce958a6b",
+    "url": "/thesis-presentation/static/js/runtime-main.5a70276e.js"
+  },
+  {
+    "revision": "6521d7ea78448ebbe4bc9d5d634e4079",
+    "url": "/thesis-presentation/static/media/Exo2.0-Light.6521d7ea.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thesis-presentation/precache-manifest.dba8b00c9dc7c104ec2ed91a8d5da072.js"
+  "/thesis-presentation/precache-manifest.cfbd8108b560760e448ebf5d9459211f.js"
 );
 
 self.addEventListener('message', (event) => {
